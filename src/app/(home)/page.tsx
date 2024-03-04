ab975ccf86f396c6e3f2205ac67014830e290e82
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-03-04 18:26:16 -0300

    style: add shadcn-ui-theme

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,13 +1,13 @@
 import { CircleDollarSign } from "lucide-react";
 
-import { LoginForm } from "@/components/login-form";
+import { LoginForm } from "@/app/(home)/_components/login-form";
 import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
     <div>
       <header className="flex items-center justify-center gap-2 py-10">
-        <CircleDollarSign size={40} />
+        <CircleDollarSign size={40} className="text-primary" />
         <h1 className="text-2xl font-bold">Money Manager</h1>
       </header>
       <div className="fixed right-4 top-4">
