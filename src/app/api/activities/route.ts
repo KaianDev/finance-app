62e47fbd115b2513d2f802cedd93001f9ca77523
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-03-06 14:44:36 -0300

    fix: handle next return error

--- src/app/api/activities/route.ts
+++ src/app/api/activities/route.ts
@@ -12,15 +12,13 @@ export const GET = async (request: NextRequest) => {
   const authToken = getToken(request);
 
   try {
-    if (authToken) {
-      const results = await backendApi.get("/activity/getAll", {
-        headers: {
-          Authorization: `Bearer ${authToken}`
-        }
-      });
-      const activities = results.data as IBackendResponseGET;
-      return new NextResponse(JSON.stringify(activities), { status: 200 });
-    }
+    const results = await backendApi.get("/activity/getAll", {
+      headers: {
+        Authorization: `Bearer ${authToken}`
+      }
+    });
+    const activities = results.data as IBackendResponseGET;
+    return new NextResponse(JSON.stringify(activities), { status: 200 });
   } catch (e) {
     return new NextResponse(JSON.stringify({ message: "Ocorreu um erro" }), {
       status: 500
