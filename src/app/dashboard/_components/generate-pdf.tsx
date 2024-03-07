2bfd75a8fb20e01e8d466ba8f4749a968be387e9
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-03-07 14:53:14 -0300

    fix: ensures the user open pdf file another window

--- src/app/dashboard/_components/generate-pdf.tsx
+++ src/app/dashboard/_components/generate-pdf.tsx
@@ -1,20 +1,29 @@
 "use client";
+import { FileText } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
-// import { usePdf } from "@/context/pdf.context";
 import { getPDF } from "@/data/activities";
-// import { useRouter } from "next/navigation";
 
 export const GeneratePDF = () => {
-  // const { setPdf } = usePdf();
-  // const router = useRouter();
   const handleGeneratePDFClick = async () => {
     const pdf = await getPDF();
     if (pdf) {
-      // setPdf(pdf);
-      // router.push("/pdf")
-      window.open(pdf);
+      const debugBase64 = (base64URL: string) => {
+        const win = window.open();
+
+        win?.document.write(`<iframe
+        src="${base64URL}"
+        frameBorder={0}
+        style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>`);
+      };
+      debugBase64(pdf);
     }
   };
 
-  return <Button onClick={handleGeneratePDFClick}>Gerar Pdf</Button>;
+  return (
+    <Button size="sm" onClick={handleGeneratePDFClick}>
+      Gerar Pdf
+      <FileText className="ml-2 size-4" />
+    </Button>
+  );
 };
