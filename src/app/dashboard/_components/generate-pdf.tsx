ce1bc882ad3f447b660b377584d084b027b791b8
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-03-08 16:43:42 -0300

    refactor: remove pdf context and pdf page

--- src/app/dashboard/_components/generate-pdf.tsx
+++ src/app/dashboard/_components/generate-pdf.tsx
@@ -1,28 +1,13 @@
 "use client";
 import { FileText } from "lucide-react";
 
-// import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
-// import { usePdf } from "@/context/pdf.context";
 import { getPDF } from "@/data/activities";
 
 export const GeneratePDF = () => {
-  // const router = useRouter();
-  // const { setPdf } = usePdf();
   const handleGeneratePDFClick = async () => {
     const pdf = await getPDF();
     if (pdf) {
-      // const debugBase64 = (base64URL: string) => {
-      //   const win = window.open();
-
-      //   win?.document.write(`<iframe
-      //   src="${base64URL}"
-      //   frameBorder={0}
-      //   style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>`);
-      // };
-      // debugBase64(pdf);
-      // setPdf(pdf);
-      // router.push("/dashboard/pdf");
       const timestamp = new Date().getTime();
       const link = document.createElement("a");
       link.download = "filename_activities" + timestamp;
