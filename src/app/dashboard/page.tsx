6034132f41b592f6eca52c01c2ebe8201e0cc95e
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-03-15 14:53:28 -0300

    style: update container margins on dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,7 +5,7 @@ import { GeneratePDF } from "./_components/generate-pdf";
 
 const DashboardPage = () => {
   return (
-    <div className="container mt-5 p-5">
+    <div className="container mt-5 px-2 py-5 sm:px-5">
       <div className="rounded-md bg-zinc-200 p-5 shadow-lg shadow-black/20 dark:bg-secondary">
         <ActivityForm />
         <ActivityDataTable />
