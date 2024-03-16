805612e23cb7645cc6f80b880c7230b251ccfb19
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2024-03-16 16:14:58 -0300

    style: adjustment styles skeleton for mobile and small screens

--- src/app/dashboard/_components/skeleton-table.tsx
+++ src/app/dashboard/_components/skeleton-table.tsx
@@ -1,59 +1,32 @@
 import { Skeleton } from "@/components/ui/skeleton";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow
-} from "@/components/ui/table";
+import { cn } from "@/lib/utils";
 
 export const SkeletonTable = () => {
   const skeletonArray = Array.from({ length: 5 }).map((_, i) => i);
 
   return (
     <div className="py-5">
-      <Table>
-        <TableHeader>
-          <TableRow>
-            {skeletonArray.map(item => (
-              <TableHead key={item}>
-                <Skeleton className="h-[40px] w-full bg-zinc-800/60 dark:bg-zinc-900/60" />
-              </TableHead>
-            ))}
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            {skeletonArray.map(item => (
-              <TableCell key={item}>
-                <Skeleton className="h-[33px] w-full bg-zinc-800/30 dark:bg-zinc-600/40" />
-              </TableCell>
-            ))}
-          </TableRow>
-          <TableRow>
-            {skeletonArray.map(item => (
-              <TableCell key={item}>
-                <Skeleton className="h-[33px] w-full bg-zinc-800/30 dark:bg-zinc-600/40" />
-              </TableCell>
-            ))}
-          </TableRow>
-          <TableRow>
-            {skeletonArray.map(item => (
-              <TableCell key={item}>
-                <Skeleton className="h-[33px] w-full bg-zinc-800/30 dark:bg-zinc-600/40" />
-              </TableCell>
-            ))}
-          </TableRow>
-          <TableRow>
-            {skeletonArray.map(item => (
-              <TableCell key={item}>
-                <Skeleton className="h-[33px] w-full bg-zinc-800/30 dark:bg-zinc-600/40" />
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableBody>
-      </Table>
+      <Skeleton className="mb-5 h-[33px] w-[99px] bg-zinc-800/30 dark:bg-zinc-600/40" />
+      <div className="grid grid-cols-1 gap-2 sm:grid-cols-5">
+        {skeletonArray.map(item => (
+          <Skeleton
+            key={item}
+            className={cn(
+              "h-[40px] w-full bg-zinc-800/60 dark:bg-zinc-900/60",
+              item !== 0 &&
+                "bg-zinc-800/30 dark:bg-zinc-600/40 sm:bg-zinc-800/60 sm:dark:bg-zinc-900/60"
+            )}
+          />
+        ))}
+        {skeletonArray.map((item, _, array) =>
+          array.map(i => (
+            <Skeleton
+              key={`#${i}.${item}`}
+              className="hidden h-[33px] w-full bg-zinc-800/30 dark:bg-zinc-600/40 sm:block"
+            />
+          ))
+        )}
+      </div>
     </div>
   );
 };
---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <div>Carregando...</div>
+    </div>
+  );
+};
+
+export default Loading;
